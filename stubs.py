99cec5f389fdefb3d54e35c9261f9c10560bb6b6
Daniel Wahlquist <triforce63@icloud.com>
2025-06-11 14:39:36 -0600

    added comments

--- stubs.py
+++ stubs.py
@@ -1,10 +1,12 @@
 
 
 def get_int():
+    # will prompt the user for an integer
     number = 10
     return number
 
 def convert(number):
+    # calls each calculation and passes number to be calculated
     binary = calc_bin(number)
     octal = calc_oct(number)
     hex = calc_hex(number)
@@ -12,21 +14,27 @@ def convert(number):
     return binary, octal, hex
 
 def calc_bin(number):
+    # convert number to binary
     return "0b11"
 
 def calc_oct(number):
+    # convert number to octal
     return "o357"
 
 def calc_hex(number):
+    #convert number to hex
     return "0Xf3"
 
 def display(number, binary, octal, hex):
+    # displays all the numbers
     print(number, binary, octal, hex)
 
 def test_conversions():
+    #test will be made later
     pass
 
 def main():
+    #driver function, to rull all the code
     number = get_int()
     binary, octal, hex = convert(number)
     display(number, binary, octal, hex)
